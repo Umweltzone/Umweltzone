eb2f635e765c5b4beb680ca86e879a48be9f4bec
Tobias Preuss <tobias.preuss@googlemail.com>
2013-10-02 23:03:28 +0200

    Add view pager to MainActivity.
    + Move map related code into MapFragment.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/activities/MainActivity.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/activities/MainActivity.java
@@ -1,85 +1,22 @@
 package de.avpptr.umweltzone.activities;
 
-import android.app.Dialog;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-
 import de.avpptr.umweltzone.R;
-import de.avpptr.umweltzone.utils.MapDrawer;
-import de.avpptr.umweltzone.utils.PointsProvider;
+import de.avpptr.umweltzone.adapters.PagerAdapter;
 
 public class MainActivity extends FragmentActivity {
-
-    private GoogleMap mMap;
-    private MapDrawer mMapDrawer;
-    private final GoogleMap.OnCameraChangeListener mOnCameraChangeListener;
-
-    public MainActivity() {
-        this.mOnCameraChangeListener = new OnCameraChangeListener();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_map);
-        setUpMapIfNeeded();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
-
-    private void setUpMapIfNeeded() {
-        if (mMap == null) {
-            int connectionResult = GooglePlayServicesUtil
-                    .isGooglePlayServicesAvailable(getApplicationContext());
-            if (connectionResult != ConnectionResult.SUCCESS) {
-                showGooglePlayServicesErrorDialog(connectionResult);
-            } else {
-                mMap = ((SupportMapFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.map)).getMap();
-                if (mMap != null) {
-                    onMapIsSetUp();
-                }
-            }
-        }
-    }
-
-    private void showGooglePlayServicesErrorDialog(int errorCode) {
-        final Dialog dialog = GooglePlayServicesUtil.getErrorDialog(errorCode, this, 0);
-        if (dialog == null) {
-            Log.e(getClass().getName(), "GooglePlayServicesErrorDialog is null.");
-        } else {
-            dialog.show();
-        }
-    }
-
-    protected void onMapIsSetUp() {
-        mMapDrawer = new MapDrawer(mMap);
-        mMap.setOnCameraChangeListener(mOnCameraChangeListener);
-        mMap.setMyLocationEnabled(true);
-    }
-
-
-    private void drawPolygonOverlay() {
-        Iterable<LatLng> points = PointsProvider.getPoints(PointsProvider.Location.Berlin);
-        Resources resources = getResources();
-        int fillColor = resources.getColor(R.color.shape_fill_color);
-        int strokeColor = resources.getColor(R.color.shape_stroke_color);
-        int strokeWidth = resources.getInteger(R.integer.shape_stroke_width);
-        mMapDrawer.drawPolygon(points, fillColor, strokeColor, strokeWidth);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
     }
 
     @Override
@@ -87,12 +24,6 @@ public class MainActivity extends FragmentActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-    class OnCameraChangeListener implements GoogleMap.OnCameraChangeListener {
-        @Override public void onCameraChange(CameraPosition cameraPosition) {
-            drawPolygonOverlay();
-        }
     }
 
 }
