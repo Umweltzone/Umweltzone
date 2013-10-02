00375079eaf19462cd00e94e3c59e044fd160c5c
ptrv <mail@petervasil.net>
2013-10-02 20:31:10 +0200

    Clear zone polygon before redraw

--- Umweltzone/src/main/java/de/avpptr/umweltzone/utils/MapDrawer.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/utils/MapDrawer.java
@@ -2,18 +2,25 @@ package de.avpptr.umweltzone.utils;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 
 public class MapDrawer {
 
     protected GoogleMap mMap;
+    protected Polygon mZonePolygon;
 
     public MapDrawer(GoogleMap map) {
         mMap = map;
     }
 
     public void drawPolygon(Iterable<LatLng> points, int fillColor, int strokeColor, int strokeWidth) {
-        mMap.addPolygon(new PolygonOptions()
+        if (mZonePolygon != null) {
+            mZonePolygon.remove();
+            mZonePolygon = null;
+        }
+
+        mZonePolygon = mMap.addPolygon(new PolygonOptions()
                 .addAll(points)
                 .strokeWidth(strokeWidth)
                 .fillColor(fillColor)
