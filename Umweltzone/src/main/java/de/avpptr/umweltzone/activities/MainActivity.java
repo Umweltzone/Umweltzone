7a5019f97572aaed5b296df0bb73a7471b1bc70d
Tobias Preuss <tobias.preuss@googlemail.com>
2013-10-03 00:47:01 +0200

    Add view pager tabs.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/activities/MainActivity.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/activities/MainActivity.java
@@ -1,22 +1,38 @@
 package de.avpptr.umweltzone.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
 import de.avpptr.umweltzone.R;
 import de.avpptr.umweltzone.adapters.PagerAdapter;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
+
+    private ViewPager mViewPager;
+    private ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
         PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(pagerAdapter);
+        mViewPager.setAdapter(pagerAdapter);
+        mViewPager.setOnPageChangeListener(new OnPageChangeListener());
+        mActionBar = getSupportActionBar();
+        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        final ActionBar.TabListener tabListener = new TabListener();
+
+        for (int tabIndex = 0; tabIndex < pagerAdapter.getCount(); ++tabIndex) {
+            ActionBar.Tab tab = mActionBar.newTab()
+                    .setText("Tab " + (tabIndex + 1))
+                    .setTabListener(tabListener);
+            mActionBar.addTab(tab);
+        }
     }
 
     @Override
@@ -24,6 +40,28 @@ public class MainActivity extends FragmentActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    class TabListener implements ActionBar.TabListener {
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+            mViewPager.setCurrentItem(tab.getPosition());
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        }
+    }
+
+    class OnPageChangeListener extends ViewPager.SimpleOnPageChangeListener {
+        @Override public void onPageSelected(int position) {
+            mActionBar.setSelectedNavigationItem(position);
+        }
     }
 
 }
