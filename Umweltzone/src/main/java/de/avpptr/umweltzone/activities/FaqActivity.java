fa2c8095f4bd432f2fed4d3a0f72bc326f6a647b
Tobias Preuss <tobias.preuss@googlemail.com>
2013-10-08 17:51:10 +0200

    Fix missing action menu in FaqActivity.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/activities/FaqActivity.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/activities/FaqActivity.java
@@ -27,6 +27,7 @@ public class FaqActivity extends BaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
         return true;
     }
 
