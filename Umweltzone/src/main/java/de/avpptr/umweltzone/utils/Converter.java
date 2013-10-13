fdd403b1956294e1c403775068f19354f37f6a41
Tobias Preuss <tobias.preuss@googlemail.com>
2013-10-13 18:24:23 +0200

    Remove XML data in favor of using JSON content.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/utils/Converter.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/utils/Converter.java
@@ -3,8 +3,6 @@ package de.avpptr.umweltzone.utils;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 
-import de.avpptr.umweltzone.R;
-
 public class Converter {
 
     // Tip for how to obtain an array of arrays from an android resource:
@@ -28,32 +26,7 @@ public class Converter {
         return geographicLocations;
     }
 
-    public static BoundingBox cityNameToBoundingBox(Resources resources, String cityName) {
-        String[] cityNames = resources.getStringArray(R.array.city_names_values);
-        int[][] zoneBounds = getGeographicLocations(resources, R.array.low_emmision_zone_bboxes);
-        if (zoneBounds.length != cityNames.length) {
-            throw new IllegalArgumentException("City names and zone bboxes sizes differ.");
-        }
-
-        for (int i = 0; i < cityNames.length; i++) {
-            if (cityName.equalsIgnoreCase(cityNames[i])) {
-                if (zoneBounds[i].length != 4) {
-                    throw new IllegalArgumentException("A bounding box should have 4 values.");
-                }
-                GeoPoint southwest = new GeoPoint(
-                        zoneBounds[i][0],
-                        zoneBounds[i][1]
-                );
-                GeoPoint northeast = new GeoPoint(
-                        zoneBounds[i][2],
-                        zoneBounds[i][3]
-                );
-                return new BoundingBox(southwest, northeast);
-            }
-        }
-        return null;
-    }
-
+    // TODO Redundant definition: City names are already present in JSON file
     public static PointsProvider.Location cityNameToLocation(String cityName) {
         PointsProvider.Location location = null;
         if (cityName.equalsIgnoreCase("Berlin")) {
