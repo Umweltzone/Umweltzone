db0b4d7f743a283311deca8f27e172c1199bae1c
Tobias Preuss <tobias.preuss@googlemail.com>
2013-10-28 11:17:12 +0100

    Remove unused method `getGeographicLocations()`.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/utils/Converter.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/utils/Converter.java
@@ -17,31 +17,7 @@
 
 package de.avpptr.umweltzone.utils;
 
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-
 public class Converter {
-
-    // Tip for how to obtain an array of arrays from an android resource:
-    // http://stackoverflow.com/a/5931094/464831
-    private static int[][] getGeographicLocations(Resources resources, int resourceId) {
-        TypedArray ta = resources.obtainTypedArray(resourceId);
-        int n = ta.length();
-        int[][] geographicLocations = new int[n][];
-
-        for (int i = 0; i < n; i++) {
-            int id = ta.getResourceId(i, 0);
-            if (id > 0) {
-                geographicLocations[i] = resources.getIntArray(id);
-            } else {
-                // something wrong with the XML
-                throw new IllegalAccessError("Something is wrong with the XML.");
-            }
-        }
-        ta.recycle(); // Important!
-
-        return geographicLocations;
-    }
 
     // TODO Redundant definition: City names are already present in JSON file
     public static PointsProvider.Location cityNameToLocation(String cityName) {
