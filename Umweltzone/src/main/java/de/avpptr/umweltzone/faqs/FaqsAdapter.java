aa08bb2feb32e34a661d798a29803ba295660d3f
Tobias Preuss <tobias.preuss@googlemail.com>
2014-06-08 16:15:39 +0200

    Format code & organize imports - no code change here.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/faqs/FaqsAdapter.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/faqs/FaqsAdapter.java
@@ -22,7 +22,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
