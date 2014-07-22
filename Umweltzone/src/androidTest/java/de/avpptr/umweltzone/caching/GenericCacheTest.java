34bcc7e1380b5b48e770e71bfd92f6eeadfa66c4
daus-salar <lars@sadau-online.de>
2014-07-22 22:11:28 +0200

    a running test case scenario with a failing test
    use
    > gradlew cAT
    or
    > gradle connectedAndroidTest
    
     and a be sure to have a connected device or a fast emulator running

---
+++ Umweltzone/src/androidTest/java/de/avpptr/umweltzone/caching/GenericCacheTest.java
@@ -0,0 +1,14 @@
+package de.avpptr.umweltzone.caching;
+
+import android.content.Context;
+
+import junit.framework.TestCase;
+
+public class GenericCacheTest extends TestCase {
+
+
+
+    public void testInitialize() throws Exception {
+        fail("Not implemented");
+    }
+}
