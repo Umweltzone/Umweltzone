54b655c59ba733944aeacc18668cd8aa2ac522cc
Tobias Preuss <tobias.preuss@googlemail.com>
2016-10-29 01:14:32 +0200

    Match annotations of base class.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/cities/CityListAdapter.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/cities/CityListAdapter.java
@@ -24,6 +24,8 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,9 +57,9 @@ public class CityListAdapter extends ArrayAdapter<LowEmissionZone> {
         mTracking = Umweltzone.getTracker();
     }
 
-
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view = convertView;
         ViewHolder viewHolder;
         if (view == null) {
