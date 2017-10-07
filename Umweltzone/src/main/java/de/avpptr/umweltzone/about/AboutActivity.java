745993c6387dab34ef06acf964d04b1746d4c7dc
Tobias Preuss <tobias.preuss@googlemail.com>
2017-10-07 17:58:37 +0200

    Use snackengage-playrate v.0.14.
    + Force to use support library v.24.2.1 instead of v.25.3.1.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/about/AboutActivity.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/about/AboutActivity.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016  Tobias Preuss
+ *  Copyright (C) 2017  Tobias Preuss
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -74,6 +74,10 @@ public class AboutActivity extends BaseActivity {
                 R.string.appinfo_references_name_parceler,
                 R.string.appinfo_references_url_parceler,
                 TrackingPoint.AboutItemClick, "parceler");
+        ViewHelper.setupTextViewExtended(this, R.id.app_info_snack_engage,
+                R.string.appinfo_references_name_snack_engage,
+                R.string.appinfo_references_url_snack_engage,
+                TrackingPoint.AboutItemClick, "snack_engage");
         ViewHelper.setupTextViewExtended(this, R.id.app_info_trace_droid,
                 R.string.appinfo_references_name_trace_droid,
                 R.string.appinfo_references_url_trace_droid,
