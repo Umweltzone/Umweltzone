5a7fcb3ffc0cc3ffb7e5f8f2a4e46dbe80bb2582
Tobias Preuss <tobias.preuss@googlemail.com>
2019-02-09 22:16:20 +0100

    Convert LowEmissionZoneNumberConverter to Kotlin.

---
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/utils/LowEmissionZoneNumberConverter.kt
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (C) 2019  Tobias Preuss
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+@file:JvmName("LowEmissionZoneNumberConverter")
+
+package de.avpptr.umweltzone.utils
+
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+
+import de.avpptr.umweltzone.R
+import de.avpptr.umweltzone.contract.LowEmissionZoneNumbers
+import de.avpptr.umweltzone.contract.Resources
+
+val @receiver:LowEmissionZoneNumbers.Color Int.statusDrawable
+    @DrawableRes get() = when (this) {
+        LowEmissionZoneNumbers.RED -> R.drawable.umweltzone_status_2
+        LowEmissionZoneNumbers.YELLOW -> R.drawable.umweltzone_status_3
+        LowEmissionZoneNumbers.GREEN -> R.drawable.umweltzone_status_4
+        else -> Resources.INVALID_RESOURCE_ID
+    }
+
+val @receiver:LowEmissionZoneNumbers.Color Int.color
+    @ColorRes get() = when (this) {
+        LowEmissionZoneNumbers.RED -> R.color.city_zone_2
+        LowEmissionZoneNumbers.YELLOW -> R.color.city_zone_3
+        LowEmissionZoneNumbers.GREEN -> R.color.city_zone_4
+        else -> R.color.city_zone_none
+    }
+
+val @receiver:LowEmissionZoneNumbers.Color Int.colorString
+    @StringRes get() = when (this) {
+        LowEmissionZoneNumbers.RED -> R.string.city_info_zone_number_since_text_fragment_red
+        LowEmissionZoneNumbers.YELLOW -> R.string.city_info_zone_number_since_text_fragment_yellow
+        LowEmissionZoneNumbers.GREEN -> R.string.city_info_zone_number_since_text_fragment_green
+        else -> Resources.INVALID_RESOURCE_ID
+    }
+
--- Umweltzone/src/main/java/de/avpptr/umweltzone/zones/dataconverters/ChildZoneExtensions.kt
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/zones/dataconverters/ChildZoneExtensions.kt
@@ -18,17 +18,10 @@
 package de.avpptr.umweltzone.zones.dataconverters
 
 import android.content.Context
-import android.support.annotation.ColorInt
 import de.avpptr.umweltzone.extensions.getColorCompat
 import de.avpptr.umweltzone.models.ChildZone
-import de.avpptr.umweltzone.utils.LowEmissionZoneNumberConverter
+import de.avpptr.umweltzone.utils.color
 import de.avpptr.umweltzone.zones.viewmodels.BadgeViewModel
 
 fun ChildZone.toBadgeViewModel(context: Context) =
-        BadgeViewModel("$zoneNumber", getBadgeColor(context))
-
-@ColorInt
-private fun ChildZone.getBadgeColor(context: Context): Int {
-    val color = LowEmissionZoneNumberConverter.getColor(zoneNumber)
-    return context.getColorCompat(color)
-}
+        BadgeViewModel("$zoneNumber", context.getColorCompat(zoneNumber.color))
