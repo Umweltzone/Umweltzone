be1f246f38234ea74fd66e5f935e0ed05ffec240
Tobias Preuss <tobias.preuss@googlemail.com>
2019-04-03 00:12:24 +0200

    Suppress false lint suggestion which is not available in current API level.

--- Umweltzone/src/main/java/de/avpptr/umweltzone/utils/BoundingBox.java
+++ Umweltzone/src/main/java/de/avpptr/umweltzone/utils/BoundingBox.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016  Tobias Preuss
+ *  Copyright (C) 2019  Tobias Preuss
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -77,7 +77,7 @@ public class BoundingBox {
         return new BoundingBox(invalidLocation, invalidLocation);
     }
 
-    @SuppressWarnings("SimplifiableIfStatement")
+    @SuppressWarnings({"SimplifiableIfStatement", "EqualsReplaceableByObjectsCall"})
     @Override
     public boolean equals(Object other) {
         if (this == other) {
