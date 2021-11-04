6e76df80cac1826d362a0e9b7c7326fbbfcc4e73
Tobias Preuss <tobias.preuss@googlemail.com>
2021-11-04 18:49:26 +0100

    Use property access syntax.

--- Umweltzone/src/test/java/de/avpptr/umweltzone/models/extensions/CameraPositionExtensionsTest.kt
+++ Umweltzone/src/test/java/de/avpptr/umweltzone/models/extensions/CameraPositionExtensionsTest.kt
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) 2021  Tobias Preuss
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.avpptr.umweltzone.models.extensions
 
 import com.google.android.gms.maps.model.CameraPosition
@@ -12,12 +29,12 @@ class CameraPositionExtensionsTest {
 
     @Test
     fun isValidWithValidZoom() {
-        assertThat(CameraPosition(LatLng(-90.0, -180.0), 0.1f, 0f, 0f).isValid()).isTrue()
+        assertThat(CameraPosition(LatLng(-90.0, -180.0), 0.1f, 0f, 0f).isValid()).isTrue
     }
 
     @Test
     fun isValidWithInvalidZoom() {
-        assertThat(CameraPosition(LatLng(-90.0, -180.0), 0.0f, 0f, 0f).isValid()).isFalse()
+        assertThat(CameraPosition(LatLng(-90.0, -180.0), 0.0f, 0f, 0f).isValid()).isFalse
     }
 
 }
